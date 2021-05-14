b2133976c266c84c5dae74bfdf56abf96116b04b
Mobin Munir <mmobinbutt@gmail.com>
2021-05-14 05:22:08 +0500

    Docs improved on FileValidator.kt.

--- pixel/src/main/java/io/pixel/utils/validators/FileValidator.kt
+++ pixel/src/main/java/io/pixel/utils/validators/FileValidator.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 object FileValidator {
     /**
-     * This method fixes the file path to exactly match the (android.net) URI to avoid file not found.
+     * This method fixes the file path to exactly match the (android.net) URI specifications to effectively prevent file not found exception.
      * @param file file to evaluate.
      * @return if the file is valid by all means, it's path.
      */
