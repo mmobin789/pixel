131fc2c8d42baf6c2f95fb95f9227de2f7f8b81b
Mobin Munir <mmobinbutt@gmail.com>
2021-12-25 00:07:49 +0500

    merge conflicts fixed.

--- sample/src/main/java/io/pixel/sample/unsplash/viewmodel/UnsplashViewModel.kt
+++ sample/src/main/java/io/pixel/sample/unsplash/viewmodel/UnsplashViewModel.kt
@@ -5,15 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.keenencharles.unsplash.Unsplash
-import com.keenencharles.unsplash.api.Order
+import com.keenencharles.unsplash.models.Order
 import com.keenencharles.unsplash.models.Photo
-
 
 class UnsplashViewModel : ViewModel() {
 
     private val unsplash = Unsplash("LKKIM-pzVTHV1XabCIik1V4LHf-Zn0x7t7lzv8-Q9J4")
 
-    private val collectionsLiveData = MutableLiveData<MutableList<Collection>>()
+    private val collectionsLiveData = MutableLiveData<MutableList<Photo>>()
     private val errorLiveData = MutableLiveData<String?>()
     private var loading = false
     private var collectionsPageNo = 1
@@ -29,16 +28,13 @@ class UnsplashViewModel : ViewModel() {
         sampleView.onPhotoCollectionError(it)
     }
 
-
     fun getCollectionsLiveData() = collectionsLiveData as LiveData<MutableList<Photo>>
 
     fun getErrorLiveData() = errorLiveData as LiveData<String?>
 
-
     fun attachView(sampleView: SampleView) {
         this.sampleView = sampleView
     }
-
 
     fun getCollections(
         perPageItems: Int = 20
@@ -54,13 +50,10 @@ class UnsplashViewModel : ViewModel() {
             onComplete = {
                 loading = false
                 collectionsLiveData.postValue(it.toMutableList())
-
-
             }, onError = {
-                errorLiveData.postValue(it)
-            })
-
-
+            errorLiveData.postValue(it)
+        }
+        )
     }
 
     interface SampleView {
